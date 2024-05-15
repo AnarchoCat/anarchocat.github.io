32afb89ebd18e8fdbeaf6d7c7fb11d2a52367c48
AnarchoCat <d2717p@gmail.com>
2024-05-15 09:30:32 +0900

    implement templating

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,13 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import  Template  from './template.ts'
+import templateString from './template.html?raw'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let template: any = new Template(templateString);
+template.viteLogo = viteLogo;
+template.typescriptLogo = typescriptLogo;
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = template.render();
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
--- src/template.ts
+++ src/template.ts
@@ -1,3 +1,36 @@
-class template {
+"use strict";
+
+class Template {
+    static pattern: RegExp = new RegExp(/\{\{\s*(\w+)\s*\}\}/, 'g');
+    #templateString: string;
+    #map: Map<string, string>;
+
+
+    constructor(templateString: string) {
+        this.#map = new Map<string, string>();
+        this.#templateString = templateString;
+        for (const m of this.#templateString.matchAll(Template.pattern)) {
+            try {
+                Reflect.defineProperty(this, m[1], {
+                    set(x) {
+                        this.#map.set(m[0], x);
+                    }
+                });
+            }
+            catch {
+
+            }
+        }
+    }
+    
+    render() {
+        let html: string = this.#templateString;
+        this.#map.forEach((v, k) => {
+            html = html.replace(k, v);
+        })
+        return html;
+    }
 
 }
+
+export default Template;
